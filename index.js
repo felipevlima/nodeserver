e1a22bac5fc61d8a3aea732b6469d726b3b4575a
Felipe Lima <felip.3lima@hotmail.com>
2021-04-29 00:41:36 -0300

    Full nlw Project

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const serverData = require('./server.json');
+const cors = require('cors');
 
 const app = express();
 
 const port = 8080;
 
 app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hellos')
+})
 
 app.get('/episodes', (req, res) => {
   res.send(serverData);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port)
